967d4155f521795677e0751bd82e4e29cd8b5114
ltherisod <lauratherisod@gmail.com>
2022-11-26 18:57:35 -0300

    added dinamic tables

--- components/table-component.js
+++ components/table-component.js
@@ -17,31 +17,12 @@ Vue.component("table-component",{
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <th scope="row">{{products[0].id}} </th>
-        <td>{{products[0].name}}</td>
-        <td>{{products[0].ingredients}}</td>
-        <td>$ {{products[0].price}}</td>
-      </tr>
-      <tr>
-        <th scope="row">{{products[1].id}} </th>
-        <td>{{products[1].name}}</td>
-        <td>{{products[1].ingredients}}</td>
-        <td>$ {{products[1].price}}</td>
-      </tr>
-      <tr>
-        <th scope="row">{{products[2].id}} </th>
-        <td>{{products[2].name}}</td>
-        <td>{{products[2].ingredients}}</td>
-        <td>$ {{products[2].price}}</td>
-      </tr>
-      <tr>
-        
-        <th scope="row">{{products[3].id}} </th>
-        <td>{{products[3].name}}</td>
-        <td>{{products[3].ingredients}}</td>
-        <td>$ {{products[3].price}}</td>
-      </tr>
+    <tr v-for="prod in this.products">
+    <th scope="row">{{prod.id}}</th>
+    <td>{{prod.name}}</td>
+    <td>{{prod.ingredients}}</td>
+    <td>$ {{prod.price}}</td>
+    </tr>
     </tbody>
   </table>
     </div>
